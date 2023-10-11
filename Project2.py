227221e5c8f1b7b692b1f37ea370a42aad914cb4
Ishva <67134497+hiishva@users.noreply.github.com>
2023-10-10 22:57:39 -0500

    File generating

--- Project2.py
+++ Project2.py
@@ -1,5 +1,8 @@
 import numpy as np
+import glob
 import pandas as pd
+import sys
+import os
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -9,30 +12,72 @@ from sklearn.metrics import accuracy_score
 from sklearn import metrics
 
 
+
+
+
+'''
+generate datasets
+create model
+tune model
+test model
+driver function
+
+'''
+def ReadFromFolder(directoryPath):
+    print('Inside readfromfolder')
+    testDataset = [] # All the data values for tests
+    trainDataset = [] # all the data values for training
+    validDataset = [] # all the data values for validation
+
+    for fileName in glob.glob(directoryPath+'*.csv'):
+        print('inside the for loop of the glob')
+        if 'test' in fileName:
+            x = pd.read_csv(fileName,low_memory=False)
+            print("appended to test")
+            testDataset.append(x)
+        elif 'train' in fileName:
+            x = pd.read_csv(fileName,low_memory=False)
+            print('appended to train')
+            trainDataset.append(x)
+        elif 'valid' in fileName:
+            x = pd.read_csv(fileName, low_memory=False)
+            print('append to valid')
+            validDataset.append(x)
+    return testDataset, trainDataset, validDataset
+
+
+
 def main() -> None:
-    df = pd.read_csv("all_data/train_c300_d100.csv", header=None)
-    df2 = pd.read_csv("all_data/test_c300_d100.csv", header=None)
-    #print(data.head())
+    directoryName = sys.argv[1]
+    print('directory name:{}'.format(directoryName))
+    testSet, trainSet, validSet = ReadFromFolder(directoryName)
+    print(testSet[1])
 
-    train_labels = df.iloc[:, -1].values.reshape(-1, 1)
-    train_data = df.iloc[:, :-1]
+    # trainingData = pd.read_csv("all_data/train_c300_d100.csv", header=None)
+    # testData = pd.read_csv("all_data/test_c300_d100.csv", header=None)
+    # #print(data.head())
 
-    test_labels = df2.iloc[:, -1].values.reshape(-1, 1)
-    test_data = df2.iloc[:, :-1]
-    print('Train size')
-    print(train_data.shape)
-    print(train_labels.shape)
-    print('Test size')
-    print(test_data.shape)
-    print(test_labels.shape)
+    # train_labels = trainingData.iloc[:, -1].values.reshape(-1, 1)
+    # train_data = trainingData.iloc[:, :-1]
 
-    classifier = DecisionTreeClassifier(criterion='log_loss', max_depth=10)
-    classifier.fit(train_data, train_labels)
+    # test_labels = testData.iloc[:, -1].values.reshape(-1, 1)
+    # test_data = testData.iloc[:, :-1]
+    # print('Train size')
+    # print(train_data.shape)
+    # print(train_labels.shape)
+    # print('Test size')
+    # print(test_data.shape)
+    # print(test_labels.shape)
 
-    test_pred = classifier.predict(test_data)
-    print(test_pred.shape)
+    # classifier = DecisionTreeClassifier(criterion='log_loss', max_depth=10)
+    # classifier.fit(train_data, train_labels)
 
-    print("Accuracy:", metrics.accuracy_score(test_labels, test_pred))
+    
+
+    # test_pred = classifier.predict(test_data)
+    # print(test_pred.shape)
+
+    # print("Accuracy:", metrics.accuracy_score(test_labels, test_pred))
 
     pass
 
