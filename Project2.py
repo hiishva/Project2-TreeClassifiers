f8fae1f8fcddbfcbc82ea41f74e3dd874db533d6
Ishva <67134497+hiishva@users.noreply.github.com>
2023-10-11 18:13:05 -0500

    DT classifier without validation

--- Project2.py
+++ Project2.py
@@ -24,24 +24,24 @@ driver function
 
 '''
 def ReadFromFolder(directoryPath):
-    print('Inside readfromfolder')
+    #print('Inside readfromfolder')
     testDataset = [] # All the data values for tests
     trainDataset = [] # all the data values for training
     validDataset = [] # all the data values for validation
 
-    for fileName in glob.glob(directoryPath+'*.csv'):
-        print('inside the for loop of the glob')
+    for fileName in sorted(glob.glob(directoryPath+'*.csv')):
+        #print('inside the for loop of the glob')
         if 'test' in fileName:
-            x = pd.read_csv(fileName,low_memory=False)
-            print("appended to test")
+            x = pd.read_csv(fileName,low_memory=False,header=None)
+            #print("{} appended to test".format(fileName))
             testDataset.append(x)
         elif 'train' in fileName:
-            x = pd.read_csv(fileName,low_memory=False)
-            print('appended to train')
+            x = pd.read_csv(fileName,low_memory=False,header=None)
+            #print('{} appended to train'.format(fileName))
             trainDataset.append(x)
         elif 'valid' in fileName:
-            x = pd.read_csv(fileName, low_memory=False)
-            print('append to valid')
+            x = pd.read_csv(fileName, low_memory=False,header=None)
+            #print('{} append to valid'.format(fileName))
             validDataset.append(x)
     return testDataset, trainDataset, validDataset
 
@@ -51,8 +51,28 @@ def main() -> None:
     directoryName = sys.argv[1]
     print('directory name:{}'.format(directoryName))
     testSet, trainSet, validSet = ReadFromFolder(directoryName)
-    print(testSet[1])
+    #print(testSet[1])
 
+    #Create labels
+    for training,testing in zip(trainSet, testSet) :
+        trainLabel = training.iloc[:, -1].values.reshape(-1,1)
+        trainData = training.iloc[:,:-1]
+        testLabel = testing.iloc[:,-1].values.reshape(-1,1)
+        testData = testing.iloc[:,:-1]
+
+        
+        print('Train Size: {}'.format(trainData.shape))
+        print('Test Size: {}'.format(testData.shape))
+        
+
+        decTreeClassifier = DecisionTreeClassifier()
+        decTreeClassifier.fit(trainData,trainLabel)
+
+        testPred = decTreeClassifier.predict(testData)
+        print('Prediction shape: {}'.format(testPred.shape))
+        print("Accuracy:", metrics.accuracy_score(testLabel, testPred))
+        print('-----------')
+    
     # trainingData = pd.read_csv("all_data/train_c300_d100.csv", header=None)
     # testData = pd.read_csv("all_data/test_c300_d100.csv", header=None)
     # #print(data.head())
